a34b41887c216334df6605facfb59a8088e0733f
Morteza Azad <morteza.azad@outlook.com>
2023-07-02 22:51:29 +0330

    Where-Expresion

--- Portal.Web/Controllers/CustomerController.cs
+++ Portal.Web/Controllers/CustomerController.cs
@@ -3,6 +3,12 @@ using Portal.Data;
 
 namespace Portal.Web.Controllers
 {
+    public class CustomerData
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+    }
     public class CustomerController : Controller
     {
         private readonly AdventureWorksDbContext _db;
@@ -12,11 +18,23 @@ namespace Portal.Web.Controllers
             _db = db;
         }
 
-        [Route("api/customer")]
+
+        //[Route("api/customer")]
+        [Route("Customers")]
         public IActionResult Index()
         {
-            var customers = _db.DimCustomers.Take(10);
-            return Ok(customers);
+            var q1= _db.DimCustomers
+                .Select(c=>new CustomerData{
+                    FirstName=c.FirstName,
+                    LastName=c.LastName,
+                    EmailAddress=c.EmailAddress
+                })
+                .Take(100);
+
+            var q2 = q1.Where(c => c.FirstName.StartsWith("A"));
+            var q3 = q2.Where(c => c.LastName.StartsWith("B"))
+                .ToList();
+            return View(q3);
         }
     }
 }
---
+++ Portal.Web/Controllers/SalesController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Portal.Data;
+using Portal.Data.Models;
+using System.Linq.Expressions;
+
+namespace Portal.Web.Controllers
+{
+    public static class ResellerExtensions
+    {
+        public static IQueryable<DimReseller> GetLargeResellers(this IQueryable<DimReseller> resellers)
+        {
+            return resellers.Where(x => x.NumberEmployees > 80);
+        }
+    }
+    public class SalesController : Controller
+    {
+        private readonly AdventureWorksDbContext _db;
+
+        public SalesController(AdventureWorksDbContext db)
+        {
+            _db = db;
+        }
+
+        [Route("api/resellers")]
+        public IActionResult GetLargeSellers()
+        {
+            return Ok(
+                _db.DimResellers
+                .Where(CheckLargeReseller)
+                .Count()
+                ) ;
+        }
+
+        [Route("api/resellers/lastOrders")]
+        public IActionResult GetLargeSellersLastOrders()
+        {
+            return Ok(
+                _db.DimResellers
+                //.Where(IsLargeReseller)
+                .GetLargeResellers()
+                .Select(r=>new
+                {
+                    r.ResellerName,
+                    r.LastOrderYear
+                }
+                ));
+        }
+
+        private Expression<Func<DimReseller,bool>> IsLargeReseller
+        {
+            get { return r => r.NumberEmployees > 80; }
+        }
+        private bool CheckLargeReseller(DimReseller reseller)
+        {
+            return reseller.NumberEmployees > 50;
+        }
+    }
+}
